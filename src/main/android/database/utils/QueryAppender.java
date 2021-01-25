357e83312e8fcf0375cd3066f8d3fafb17621ef1
Guilherme Eyng <guilherme_eyng@hotmail.com>
2021-01-25 15:46:00 -0300

    Adição de tratamento e Javadoc com o QueryAppender

--- src/main/android/database/utils/QueryAppender.java
+++ src/main/android/database/utils/QueryAppender.java
@@ -2,8 +2,18 @@ package main.android.database.utils;
 
 public class QueryAppender {
 
+    /**
+     * Método usado para concatenar trechos de sql.
+     * A cada parâmetro passado, é adicionado um espaço e uma quebra de linha automaticamente.
+     *
+     * @param strs são os trechos de sql a serem concatenados
+     *
+     * @return todos os parâmetros concatenados ou vazio se não for passado nada ou for passado nulo
+     */
     public static String append(String... strs){
-        return String.join(" \n", strs);
+        if(strs != null && strs.length > 0)
+            return String.join(" \n", strs);
+        return "";
     }
 
 }
